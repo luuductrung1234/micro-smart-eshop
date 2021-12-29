58665608b0d7a1e1cedb7c9d71be281485786dc8
Thomas Luu (Tom) <thomas.luu@transwap.com>
2021-12-29 10:46:47 +0700

    jpa data repository

--- src/main/java/com/example/fullstackdemo1/student/Student.java
+++ src/main/java/com/example/fullstackdemo1/student/Student.java
@@ -1,16 +1,49 @@
 package com.example.fullstackdemo1.student;
 
 import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
 
 @ToString
 @Getter
 @Setter
-@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1)
+    @GeneratedValue(
+            generator = "student_sequence",
+            strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private String email;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
+
+    public Student(String name, String email, Gender gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Student student = (Student) o;
+        return id != null && Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
