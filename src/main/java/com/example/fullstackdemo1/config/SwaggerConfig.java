8b189c149b9bb3b03fa56c0cc704dfc7daf3bee4
Thomas Luu (Tom) <thomas.luu@transwap.com>
2022-07-03 10:37:20 +0700

    migrate to mariadb & enable swagger ui

--- src/main/java/com/example/fullstackdemo1/config/SwaggerConfig.java
+++ src/main/java/com/example/fullstackdemo1/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.example.fullstackdemo1.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -9,10 +10,12 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@ComponentScan(basePackageClasses = SwaggerConfig.class)
 public class SwaggerConfig {
     @Bean
     public Docket api() {
@@ -32,5 +35,26 @@ public class SwaggerConfig {
                 .license("MIT License")
                 .licenseUrl("https://github.com/AmazingEnergy/full-stack-demo-01/blob/main/LICENSE")
                 .version("1.0.0").build();
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .showCommonExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+                .validatorUrl(null)
+                .build();
     }
 }
