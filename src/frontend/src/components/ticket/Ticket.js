d38f9a759e972fc408b6c2c82fce72e0c52207f1
Thomas Luu (Tom) <thomas.luu@transwap.com>
2022-08-10 20:21:39 +0700

    fix UI build fail

--- src/frontend/src/components/ticket/Ticket.js
+++ src/frontend/src/components/ticket/Ticket.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, useRef } from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 import classes from "./Ticket.module.css";
 
