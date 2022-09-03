fed576a1686e80346365fe4b9b0f0dfecb29e3f0
Thomas Luu (Tom) <thomas.luu@transwap.com>
2022-09-03 16:26:41 +0700

    fix seed data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into user (id, name, email, password, gender, role, location) values (1, 'Thomas Luu', 'thomas_test123@yopmail.com', '123', 'MALE', 'ADMIN', '', '', '');
-insert into user (id, name, email, password, gender, role, location) values (2, 'John Doe', 'john_test123@yopmail.com', '123', 'MALE', 'CUSTOMER', 's1,s2,u2', '', '');
-insert into user (id, name, email, password, gender, role, location) values (3, 'Alice George', 'alice_test123@yopmail.com', '123', 'FEMALE', 'CUSTOMER', 's1,s3,u3', '', '');
+insert into user (id, name, email, password, gender, role, location, environment, devices) values (1, 'Thomas Luu', 'thomas_test123@yopmail.com', '123', 'MALE', 'ADMIN', '', '', '');
+insert into user (id, name, email, password, gender, role, location, environment, devices) values (2, 'John Doe', 'john_test123@yopmail.com', '123', 'MALE', 'CUSTOMER', 's1,s2,u2', '', '');
+insert into user (id, name, email, password, gender, role, location, environment, devices) values (3, 'Alice George', 'alice_test123@yopmail.com', '123', 'FEMALE', 'CUSTOMER', 's1,s3,u3', '', '');
 
 -- incompatible with mysql8
 -- SELECT setval(student_sequence, 1000);  -- next value will be 1001
